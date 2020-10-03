94189143ac93b70ebe8e017e19d81aa558f75e48
tushx3 <tkushwaha70@gmail.com>
2020-10-03 20:46:01 +0530

    added video playback

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+	const videoRef = useRef(null);
+	const [isPlaying, setIsplaying] = useState(false);
+
+	function handleVideoClick() {
+		if (isPlaying) {
+			videoRef.current.pause();
+			setIsplaying(false);
+		} else {
+			videoRef.current.play();
+			setIsplaying(true);
+		}
+	}
+
 	return (
-		<div className="Video">
-			<div className="video_player">
-				<video src="https://vod-progressive.akamaized.net/exp=1601744410~acl=%2A%2F1224524886.mp4%2A~hmac=75767de5ce674b34508958d4b06beb93a87a5660a456f2116ee7d97031b2870d/vimeo-prod-skyfire-std-us/01/3301/12/316506213/1224524886.mp4?download=1&filename=Pexels+Videos+1893623.mp4"></video>
-			</div>
+		<div className="video">
+			<video
+				onClick={handleVideoClick}
+				ref={videoRef}
+				className="video_player"
+				src="https://cdn.videvo.net/videvo_files/video/free/2015-08/originalContent/raininglettersBW.mp4"
+			></video>
 		</div>
 	);
 }
